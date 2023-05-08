62bc633160369c921a866b3da828884c5a640dac
Ömer Osman <113211889+omerosmann@users.noreply.github.com>
2023-05-08 14:27:08 +0300

    Add files via upload

--- invertory-service/src/main/java/com/kodlamaio/invertoryservice/InvertoryServiceApplication.java
+++ invertory-service/src/main/java/com/kodlamaio/invertoryservice/InvertoryServiceApplication.java
@@ -3,13 +3,13 @@ package com.kodlamaio.invertoryservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class InvertoryServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InvertoryServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(InvertoryServiceApplication.class, args);
+    }
 
 
 }
