d54d77c3cd87cac32601c49fca5c307eb75c0288
Ömer Osman <113211889+omerosmann@users.noreply.github.com>
2023-05-19 14:38:21 +0300

    Update CarClient.java

--- rental-service/src/main/java/com/kodlamaio/rentalservice/api/clients/CarClient.java
+++ rental-service/src/main/java/com/kodlamaio/rentalservice/api/clients/CarClient.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 @FeignClient(name = "inventory-service", fallback = CarClientFallback.class)
 public interface CarClient {
 
-    //@Retry(name = "checkIfCarAvailable") !!! Fault Tolerance Limiter Different Solution !!!
+    //@Retry(name = "checkIfCarAvailable") 
     @GetMapping(value = "/api/cars/check-car-available/{carId}")
     ClientResponse checkIfCarAvailable(@PathVariable UUID carId);
 }
