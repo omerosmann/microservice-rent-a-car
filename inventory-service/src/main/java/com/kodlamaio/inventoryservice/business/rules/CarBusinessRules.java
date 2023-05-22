a4356e44a0232a7a8ffb7cf950ea168648c68018
Ömer Osman <113211889+omerosmann@users.noreply.github.com>
2023-05-22 22:33:15 +0300

    Add files via upload

---
+++ inventory-service/src/main/java/com/kodlamaio/inventoryservice/business/rules/CarBusinessRules.java
@@ -0,0 +1,25 @@
+package com.kodlamaio.inventoryservice.business.rules;
+
+import com.kodlamaio.commonpackage.utils.exception.BusinessException;
+import com.kodlamaio.inventoryservice.entities.enums.State;
+import com.kodlamaio.inventoryservice.repository.CarRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class CarBusinessRules {
+    private final CarRepository repository;
+
+    public void checkIfCarExists(UUID id) {
+        if (!repository.existsById(id)) throw new BusinessException("CAR_NOT_EXISTS");
+    }
+
+    public void checkCarAvailability(UUID id) {
+        var car = repository.findById(id).orElseThrow();
+
+        if (!car.getState().equals(State.Available)) throw new BusinessException("CAR_NOT_AVAILABLE");
+    }
+}
---
+++ inventory-service/src/test/java/com/kodlamaio/inventoryservice/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodlamaio.inventoryservice;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
