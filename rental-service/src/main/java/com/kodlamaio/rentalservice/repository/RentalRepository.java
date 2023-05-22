05f0821de4d4196d3cb25ca55877f19ae1d276fd
Ömer Osman <113211889+omerosmann@users.noreply.github.com>
2023-05-22 21:46:54 +0300

    Delete rental-service/src/main/java/com/kodlamaio/rentalservice directory

--- rental-service/src/main/java/com/kodlamaio/rentalservice/repository/RentalRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kodlamaio.rentalservice.repository;
-
-import com.kodlamaio.rentalservice.entities.Rental;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface RentalRepository extends JpaRepository<Rental, UUID> {
-}
