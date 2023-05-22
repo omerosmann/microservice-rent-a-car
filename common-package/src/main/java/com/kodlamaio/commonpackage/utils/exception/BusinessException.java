ec0365f8cc6a09bb0cda5f78cc33ca0d34d9273b
Ömer Osman <113211889+omerosmann@users.noreply.github.com>
2023-05-22 21:48:19 +0300

    Delete common-package/src/main/java/com/kodlamaio/commonpackage directory

--- common-package/src/main/java/com/kodlamaio/commonpackage/utils/exception/BusinessException.java
+++
@@ -1,7 +0,0 @@
-package com.kodlamaio.commonpackage.utils.exception;
-
-public class BusinessException extends RuntimeException {
-    public BusinessException(String message) {
-        super(message);
-    }
-}
