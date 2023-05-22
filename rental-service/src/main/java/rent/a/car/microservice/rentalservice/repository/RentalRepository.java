ff9a5daa305792cd170ff6e0dfa3b5aed3967813
Ömer Osman <113211889+omerosmann@users.noreply.github.com>
2023-05-22 22:27:13 +0300

    Delete rental-service directory

--- rental-service/src/main/java/rent/a/car/microservice/rentalservice/repository/RentalRepository.java
+++
@@ -1,8 +0,0 @@
-package rent.a.car.microservice.rentalservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import rent.a.car.microservice.rentalservice.entities.Rental;
-
-import java.util.UUID;
-
-public interface RentalRepository extends JpaRepository<Rental, UUID> {}
