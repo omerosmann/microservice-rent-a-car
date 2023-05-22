02e9db70b6335e9053897109fd28f420747f406f
Ömer Osman <113211889+omerosmann@users.noreply.github.com>
2023-05-22 22:26:44 +0300

    Delete inventory-service directory

--- inventory-service/src/main/java/rent/a/car/microservice/inventoryservice/repository/BrandRepository.java
+++
@@ -1,8 +0,0 @@
-package rent.a.car.microservice.inventoryservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import rent.a.car.microservice.inventoryservice.entities.Brand;
-
-import java.util.UUID;
-
-public interface BrandRepository extends JpaRepository<Brand, UUID> {}
--- inventory-service/src/main/java/rent/a/car/microservice/inventoryservice/repository/ModelRepository.java
+++
@@ -1,8 +0,0 @@
-package rent.a.car.microservice.inventoryservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import rent.a.car.microservice.inventoryservice.entities.Model;
-
-import java.util.UUID;
-
-public interface ModelRepository extends JpaRepository<Model, UUID>{}
