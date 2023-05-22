5b6fe06384e1795f04d5fc87056f6559bc94a439
Ömer Osman <113211889+omerosmann@users.noreply.github.com>
2023-05-22 22:28:53 +0300

    Add files via upload

---
+++ common-package/src/main/java/com/kodlamaio/commonpackage/events/payment/PaymentDeletedEvent.java
@@ -0,0 +1,17 @@
+package com.kodlamaio.commonpackage.events.payment;
+
+import com.kodlamaio.commonpackage.events.Event;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentDeletedEvent implements Event {
+    private UUID paymentId;
+}
