32bd90c76caa85371cd5aee052e69d6d7b4d5b9b
Ömer Osman <113211889+omerosmann@users.noreply.github.com>
2023-06-03 00:02:01 +0300

    Delete invoice-service directory

--- invoice-service/src/main/java/com/kodlamaio/invoice/repository/InvoiceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kodlamaio.invoice.repository;
-
-import com.kodlamaio.invoice.entities.Invoice;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface InvoiceRepository extends MongoRepository<Invoice, String> {
-}
