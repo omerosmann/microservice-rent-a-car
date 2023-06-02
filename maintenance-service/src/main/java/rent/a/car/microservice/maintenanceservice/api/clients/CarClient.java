5bccd4b96f3493b13288c744333108048b65674e
Ömer Osman <113211889+omerosmann@users.noreply.github.com>
2023-06-03 00:09:35 +0300

    Add files via upload

---
+++ maintenance-service/src/main/java/rent/a/car/microservice/maintenanceservice/api/clients/CarClient.java
@@ -0,0 +1,17 @@
+package rent.a.car.microservice.maintenanceservice.api.clients;
+
+import com.kodlamaio.commonpackage.utils.dto.ClientResponse;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.UUID;
+
+@FeignClient(name = "inventory-service", fallback = CarClientFallback.class)
+public interface CarClient {
+
+    @Retry(name = "checkIfCarAvailable")
+    @GetMapping(value = "/api/cars/check-car-available/{carId}")
+    ClientResponse checkIfCarAvailable(@PathVariable UUID carId);
+}
